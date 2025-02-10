091b625c1dbd78976daa4b7a82ee00d01bfc038c
admens2008 <adjeimensah@gmail.com>
2025-02-10 21:59:09 +0000

    multiplication time table for 2 using for loop

---
+++ mult_table.c
@@ -0,0 +1,16 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int x;
+    int y;
+    
+    printf("Multiplication table for 2: \n");
+    for (x = 1; x <= 10; x++)
+    {
+        printf("2 * %d = %d\n", x, 2 * x);
+    }
+    
+
+    return 0;
+}
